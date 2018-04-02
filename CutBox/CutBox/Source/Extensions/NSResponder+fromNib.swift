179246b4f16e7d780b320555bf34b4df1caeeabc
Jasonm23 <jasonm23@gmail.com>
2018-04-02 13:45:27 +0800

    Rename NSView+fromNib to NSResponder+fromNib
    View, Panel, Window can all use this to load from their xibs

--- CutBox/CutBox/Source/Extensions/NSResponder+fromNib.swift
+++ CutBox/CutBox/Source/Extensions/NSResponder+fromNib.swift
@@ -1,5 +1,5 @@
 //
-//  NSView+fromNib.swift
+//  NSResponder+fromNib.swift
 //  CutBox
 //
 //  Created by Jason Milkins on 25/3/18.
@@ -21,15 +21,3 @@ extension NSResponder {
         return windowArray?.first(where: { $0 is T }) as? T
     }
 }
-
-//extension NSView {
-//    class func fromNib<T: NSView>() -> T? {
-//        var viewArray: NSArray? = nil
-//        guard Bundle.main.loadNibNamed(
-//            NSNib.Name(rawValue: String(describing: T.self)),
-//            owner: nil,
-//            topLevelObjects: &viewArray) else { return nil }
-//        return viewArray?.first(where: { $0 is T }) as? T
-//    }
-//}
-
