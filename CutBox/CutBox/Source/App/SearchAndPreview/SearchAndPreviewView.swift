1d6fb78b23ecff316b38cc83adac60233ae11832
Jasonm23 <jasonm23@gmail.com>
2018-04-10 18:25:39 +0800

    Move search and preview key hander to it's own file

--- CutBox/CutBox/Source/App/SearchAndPreview/SearchAndPreviewView.swift
+++ CutBox/CutBox/Source/App/SearchAndPreview/SearchAndPreviewView.swift
@@ -9,37 +9,6 @@
 import Cocoa
 import RxSwift
 import RxCocoa
-import Carbon.HIToolbox
-
-extension SearchAndPreviewView {
-    override func keyDown(with event: NSEvent) {
-        let (keycode, modifiers) =
-            (event.keyCode,
-             event.modifierFlags.intersection(.deviceIndependentFlagsMask))
-
-        switch (keycode, modifiers) {
-
-        case (UInt16(kVK_Escape),_):
-            self.events
-                .onNext(.justClose)
-
-        case (UInt16(kVK_Return),_):
-            self.events
-                .onNext(.closeAndPaste)
-
-        case (UInt16(kVK_ANSI_S), [.command]):
-            self.events
-                .onNext(.toggleSearchMode)
-
-        case (UInt16(kVK_ANSI_S), [.command]):
-            self.events
-                .onNext(.toggleSearchMode)
-
-        default:
-            return
-        }
-    }
-}
 
 class SearchAndPreviewView: NSView {
 
---
+++ CutBox/CutBox/Source/App/SearchAndPreview/SearchAndPreview_KeyDownHandler.swift
@@ -0,0 +1,40 @@
+//
+//  SearchAndPreview_KeyDownHandler.swift
+//  CutBox
+//
+//  Created by Jason on 10/4/18.
+//  Copyright © 2018 ocodo. All rights reserved.
+//
+
+import RxSwift
+import Carbon.HIToolbox
+
+extension SearchAndPreviewView {
+    override func keyDown(with event: NSEvent) {
+        let (keycode, modifiers) =
+            (event.keyCode,
+             event.modifierFlags.intersection(.deviceIndependentFlagsMask))
+
+        switch (keycode, modifiers) {
+
+        case (UInt16(kVK_Escape),_):
+            self.events
+                .onNext(.justClose)
+
+        case (UInt16(kVK_Return),_):
+            self.events
+                .onNext(.closeAndPaste)
+
+        case (UInt16(kVK_ANSI_S), [.command]):
+            self.events
+                .onNext(.toggleSearchMode)
+
+        case (UInt16(kVK_ANSI_S), [.command]):
+            self.events
+                .onNext(.toggleSearchMode)
+
+        default:
+            return
+        }
+    }
+}
