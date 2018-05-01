1e438c10259fd7f2533cd3fa32488a479e6e6e8a
Jasonm23 <jasonm23@gmail.com>
2018-05-01 17:50:22 +0800

    Generate a dictionary instead of using 2 arrays with index parity.

--- CutBox/CutBox/Source/Utilities/FakeKeyEvents.swift
+++ CutBox/CutBox/Source/Utilities/FakeKeyEvents.swift
@@ -9,36 +9,49 @@
 import Foundation
 import Magnet
 
-func send(fakeKey key: String, useCommandFlag: Bool) {
-    let sourceRef = CGEventSource(stateID: .combinedSessionState)
-
-    let asciiRange: [UInt16] = (0...255).map { UInt16($0) }
-    let asciiMap: [UniChar] = asciiRange.map {
-
-        guard let event: CGEvent = CGEvent(
-            keyboardEventSource: sourceRef, virtualKey: $0, keyDown: true
-            ) else { fatalError("Cannot generate CGEvent for :\($0)") }
-
-        var (char, len) = (UniChar(), 0)
-
-        event.keyboardGetUnicodeString(maxStringLength: 1,
-                                       actualStringLength: &len,
-                                       unicodeString: &char)
-        return char
-    }
-
-    if let longInt = key[key.startIndex].ascii {
-        let keyUniChar: UniChar = numericCast(longInt)
-        let index = asciiMap.index(of: keyUniChar)
-        let keyCode = asciiRange[index!]
-        send(fakeKey: keyCode, useCommandFlag: useCommandFlag)
-    }
-}
-
-extension Character {
+fileprivate extension Character {
     var ascii: UInt32? {
         let asciiCode: UInt32? = String(self).unicodeScalars.filter{$0.isASCII}.first?.value
         return asciiCode
+    }
+}
+
+func send(fakeKey key: String, useCommandFlag: Bool) {
+    let sourceRef = CGEventSource(stateID: .combinedSessionState)
+
+    if sourceRef == nil {
+        NSLog("No event source")
+        return
+    }
+
+    let asciiRange: [UInt16] = (0...255).map { UInt16($0) }
+    var asciiMap: [UniChar:UInt16] = [:]
+
+    asciiRange.forEach {
+        guard let event: CGEvent = CGEvent(
+            keyboardEventSource: sourceRef,
+            virtualKey: $0,
+            keyDown: true
+            )
+
+            else { fatalError("Cannot generate CGEvent for :\($0)") }
+
+        var (char, len) = (UniChar(), 0)
+        event.keyboardGetUnicodeString(
+            maxStringLength: 1,
+            actualStringLength: &len,
+            unicodeString: &char
+        )
+
+        asciiMap[char] = $0
+    }
+
+    if let asciiCode = key[key.startIndex].ascii {
+        let keyUniChar: UniChar = numericCast(asciiCode)
+        guard let keyCode = asciiMap[keyUniChar]
+            else { fatalError("Cannot get ASCII value for key: \(key)") }
+
+        send(fakeKey: keyCode, useCommandFlag: useCommandFlag)
     }
 }
 
