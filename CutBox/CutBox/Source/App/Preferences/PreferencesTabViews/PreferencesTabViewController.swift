e564585e79476529368b192aad5880270f88c9ab
Jasonm23 <jasonm23@gmail.com>
2018-05-12 21:29:57 +0800

    Creating new preferences window

---
+++ CutBox/CutBox/Source/App/Preferences/PreferencesTabViews/PreferencesTabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesTabViewController.swift
+//  CutBox
+//
+//  Created by Jason on 12/5/18.
+//  Copyright © 2018 ocodo. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesTabViewController: NSTabViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabView = 
+    }
+    
+}
