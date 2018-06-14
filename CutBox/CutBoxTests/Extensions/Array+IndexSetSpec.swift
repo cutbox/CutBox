03b9290ff54ee28c0b5e7587401c0441e80559d6
Jasonm23 <jasonm23@gmail.com>
2018-06-14 14:52:20 +0800

    Fix swiftlint violations

--- CutBox/CutBoxTests/Extensions/Array+IndexSetSpec.swift
+++ CutBox/CutBoxTests/Extensions/Array+IndexSetSpec.swift
@@ -15,9 +15,9 @@ class Array_IndexSetSpec: QuickSpec {
     override func spec() {
         describe("Array+IndexSet") {
             it("Accesses an array using an indexset") {
-                let sut = [0,1,2,3,4,5,6,7]
-                let indexes = [1,3,6] as IndexSet
-                let expected = [1,3,6]
+                let sut = [0, 1, 2, 3, 4, 5, 6, 7]
+                let indexes = [1, 3, 6] as IndexSet
+                let expected = [1, 3, 6]
                 let actual = sut[indexes]
                 expect(expected).to(equal(actual))
             }
