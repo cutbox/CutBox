379dbd8d8f357513d756889c21f2e3dd08b9be01
Jasonm23 <jasonm23@gmail.com>
2018-06-14 14:50:06 +0800

    Fix swiftlint violations

--- CutBox/CutBox/Source/Extensions/NSImage+tint.swift
+++ CutBox/CutBox/Source/Extensions/NSImage+tint.swift
@@ -12,12 +12,14 @@ extension NSImage {
     func tint(color: NSColor) -> NSImage {
         guard self.isTemplate == false else { return self }
 
+        //swiftlint:disable force_cast
         let image = self.copy() as! NSImage
-        image.lockFocus()
+        //swiftlint:enable force_cast
 
+        image.lockFocus()
         color.set()
 
-        let imageRect = NSRect(origin: NSZeroPoint, size: image.size)
+        let imageRect = NSRect(origin: NSPoint.zero, size: image.size)
         imageRect.fill(using: .sourceAtop)
 
         image.unlockFocus()
