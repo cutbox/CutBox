e76d867827e5c74159a6fa7e75691a25871f4656
jasonm23 <jasonm23@gmail.com>
2022-05-25 18:37:47 +0700

    Add cask formula for github

--- cask/cutbox.rb
+++ cask/cutbox.rb
@@ -1,7 +1,7 @@
 cask "cutbox" do
   version "1.4.11"
 
-  sha256 "acc6df670f7922bb23c5bc472bc0a6c2fa8f36c0d9d4060f75e4a66a0f35bd4f"
+  sha256 "68ba878d6fa140d38d25fc97468129d0492b56f2b602b970e8daa992917b236e"
 
   url "https://github.com/cutbox/CutBox/releases/download/#{version}/CutBox.dmg",
       verified: "github.com/cutbox/CutBox/"
