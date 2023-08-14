41253b91c76b6bdd9637c36a22b0c6f3dd15ab88
jasonm23 <jasonm23@gmail.com>
2023-08-14 08:46:17 +0700

    New separators; add test tube emoji to header

--- bin/markdown_xcpretty_formatter.rb
+++ bin/markdown_xcpretty_formatter.rb
@@ -3,10 +3,11 @@ require 'xcpretty'
 class Markdown < XCPretty::Simple
 
   def format_test_case_name(name)
+    separator = " ➠ "
     name = name.gsub('Don_t', "Don't")
     name = name.gsub(/(\d+)_(\d+)/, '\1.\2')
-    name = name.gsub('____', ' : ')
-    name = name.gsub('__', ' : ')
+    name = name.gsub('____', separator)
+    name = name.gsub('__', separator)
     name = name.gsub('_', ' ')
     name
   end
@@ -16,7 +17,7 @@ class Markdown < XCPretty::Simple
 
 # Started: #{name}
 
-| - | Name |
+| 🧪   | Name |
 |:-----|:-----|}
   end
 
