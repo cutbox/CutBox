ee32b3366bb463708fb9629690c1930c694df6ff
jasonm23 <jasonm23@gmail.com>
2023-08-14 07:57:42 +0700

    Try new test case name formatter

--- bin/markdown_xcpretty_formatter.rb
+++ bin/markdown_xcpretty_formatter.rb
@@ -2,6 +2,14 @@ require 'xcpretty'
 
 class Markdown < XCPretty::Simple
 
+  def format_test_case_name(name)
+    name = name.gsub(/(\d+)_(\d+)/, '\1.\2')
+    name = name.gsub('_', ' ')
+    name = name.gsub('__', ' : ')
+    name = name.split(' : ').map(&:capitalize).join(' : ')
+    name
+  end
+
   def format_test_run_started(name)
     %{
 
@@ -20,12 +28,12 @@ class Markdown < XCPretty::Simple
   end
 
   def format_passing_test(suite, test_case, time)
-    "| ✓ | #{test_case} (#{time}) |"
+    "| ✓ | #{format_test_case_name(test_case)} (#{time}) |"
   end
 
 
   def format_failing_test(suite, test_case, reason, file_path)
-    %{| ✗ | #{test_case}  |
+    %{| ✗ | #{format_test_case_name(test_case)} |
 |   | #{file_path}      |
 |   | Reason: #{reason} |}
   end
