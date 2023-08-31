ef97905e4e6d59deda5fc4919ba5db397f4eb4c4
jasonm23 <jasonm23@gmail.com>
2023-08-31 16:55:54 +0700

    Add collectErrors tests for CommandParams

--- cutbox_command/Tests/CutBoxCLITests/CommandParamsTests.swift
+++ cutbox_command/Tests/CutBoxCLITests/CommandParamsTests.swift
@@ -40,17 +40,35 @@ class CommandParamsSpec: QuickSpec {
             }
 
             it("should return nil if option is not present") {
-                subject.arguments  = ["programName", "-otherOpt", "value"]
+                subject.arguments  = ["--otherOpt", "value"]
 
-                let result: String? = subject.hasOpt("-opt")
+                let result: String? = subject.hasOpt("--opt")
                 expect(result).to(beNil())
             }
 
-            it("should return nil if option value starts with '-'") {
-                subject.arguments  = ["programName", "-opt", "-invalidValue"]
+            it("should return nil if option value starts with '--'") {
+                subject.arguments  = ["--opt", "--invalidValue"]
 
-                let result: String? = subject.hasOpt("-opt")
+                let result: String? = subject.hasOpt("--opt")
                 expect(result).to(beNil())
+            }
+        }
+
+        context("after parsing, arguments left over are errors") {
+            describe("collectErrors") {
+                it("collects remaining arguments") {
+                    let arguments = ["-o0", "value1",
+                                     "-o1", "value2",
+                                     "--opt2",
+                                     "--opt3", "value2"]
+                    subject.collectErrors(arguments)
+
+                    expect(subject.errors.count) == 4
+                    expect(subject.errors[0]) == ("-o0", "value1")
+                    expect(subject.errors[1]) == ("-o1", "value2")
+                    expect(subject.errors[2]) == ("--opt3", "")
+                    expect(subject.errors[3]) == ("--opt4", "value2")
+                }
             }
         }
     }
