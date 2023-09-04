ef8f5762365c2cf53680c3e9ffd2adc1980dff68
jasonm23 <jasonm23@gmail.com>
2023-09-04 23:06:31 +0700

    Add indicator dots on left of cov filenames

---
+++ docs/js/addBlobs.js
@@ -0,0 +1,40 @@
+// Add color blobs in-front of filename in an lcov index.html. Each blob,
+// corresponds to the color status of the function, line, region coverage column.
+// Makes it easier to see the cover overview on mobile.
+
+function setColorBlobs(row) {
+  let grey = `<span class="blob grey">🞄</span>`
+  let green = `<span class="blob green">🞄</span>`
+  let red = `<span class="blob red">🞄</span>`
+  let classes = {
+    "column-entry-green": green,
+    "column-entry-yellow": grey,
+    "column-entry-red": red
+  }
+
+  let td = row.cells.item(0)
+  var blobs = [
+    1,
+    2,
+    3
+  ].map( (i) => classes[row.cells.item(i).classList[0]] )
+
+  td.innerHTML = `<nobr>${blobs.join("")}${td.innerHTML}</nobr>`
+}
+
+function ready() {
+  let t = document.querySelector('table')
+  for(var i = 1; i < t.rows.length -1; i++) {
+    setColorBlobs(t.rows.item(i))
+  }
+  console.log("Doc ready...")
+}
+
+// inject it into the index.html for a coverage folder.
+// <script src="/js/addBlobs.js"/>
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOMContentLoaded")
+  console.log(event)
+  ready()
+});
