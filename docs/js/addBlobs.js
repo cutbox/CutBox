a3f5851876c1555e66017a6142a52955a7912ebe
jasonm23 <jasonm23@gmail.com>
2023-09-06 12:11:58 +0700

    Shut codacy up over a single func js file...

--- docs/js/addBlobs.js
+++ docs/js/addBlobs.js
@@ -3,29 +3,25 @@
 // Makes it easier to see the cover overview on mobile.
 
 function setColorBlobs(row) {
-  let blobGlyph = '⬩';
+  let blobGlyph = "⬩";
   let grey = `<span class="blob grey">${blobGlyph}</span>`;
   let green = `<span class="blob green">${blobGlyph}</span>`;
   let red = `<span class="blob red">${blobGlyph}</span>`;
   let classes = {
     "column-entry-green": green,
     "column-entry-yellow": grey,
-    "column-entry-red": red
+    "column-entry-red": red,
   };
 
   let td = row.cells.item(0);
-  var blobs = [
-    1,
-    2,
-    3
-  ].map( (i) => classes[row.cells.item(i).classList[0]] );
+  var blobs = [1,2,3].map( (i) => classes[row.cells.item(i).classList[0]] );
 
   td.innerHTML = `<nobr>${blobs.join("")}${td.innerHTML}</nobr>`;
 }
 
 function ready() {
-  let t = document.querySelector('table');
-  for(var i = 1; i < t.rows.length -1; i++) {
+  let t = document.querySelector("table");
+  for(var i = 1; i < t.rows.length - 1; i++) {
     setColorBlobs(t.rows.item(i));
   }
   console.log("Doc ready...");
