61672f6090cdb8e863f10311b2aef69a34bcf7b5
jasonm23 <jasonm23@gmail.com>
2023-09-09 01:11:16 +0700

    Create onNext handler for SearchViewEvents

--- CutBox/CutBox/Source/App/SearchAndPreview/SearchViewController.swift
+++ CutBox/CutBox/Source/App/SearchAndPreview/SearchViewController.swift
@@ -209,77 +209,79 @@ class SearchViewController: NSObject {
     // swiftlint:disable function_body_length
     private func setupSearchTextEventBindings() {
         self.events
-            .subscribe(onNext: { event in
-                switch event {
-                case .setSearchMode(let mode):
-                    self.historyService.searchMode = mode
-                    self.searchView.reloadData()
-                    self.searchView.setSearchModeButton(mode: mode)
-
-                case .toggleSearchMode:
-                    let mode = self.historyService.toggleSearchMode()
-                    self.searchView.reloadData()
-                    self.searchView.setSearchModeButton(mode: mode)
-
-                case .setTimeFilter(let seconds):
-                    self.historyService.setTimeFilter(seconds: seconds)
-                    self.searchView.reloadData()
-
-                case .toggleTimeFilter:
-                    self.searchView.toggleTimeFilter()
-                    self.historyService.setTimeFilter(seconds: nil)
-                    self.searchView.reloadData()
-
-                case .cycleTheme:
-                    self.prefs.cycleTheme()
-                    self.searchView.applyTheme()
-                    self.reloadDataWithExistingSelection()
-
-                case .toggleWrappingStrings:
-                    self.prefs.useWrappingStrings.toggle()
-                    self.updateSearchItemPreview()
-
-                case .toggleJoinStrings:
-                    self.prefs.useJoinString.toggle()
-                    self.updateSearchItemPreview()
-
-                case .toggleSearchScope:
-                    self.historyService.favoritesOnly.toggle()
-                    self.searchView.reloadData()
-                    self.searchView.setSearchScopeButton(favoritesOnly: self.historyService.favoritesOnly)
-
-                case .togglePreview:
-                    self.prefs.hidePreview.toggle()
-
-                case .scaleTextDown:
-                    self.prefs.scaleTextDown()
-                    self.reloadDataWithExistingSelection()
-
-                case .scaleTextUp:
-                    self.prefs.scaleTextUp()
-                    self.reloadDataWithExistingSelection()
-
-                case .scaleTextNormalize:
-                    self.prefs.scaleTextNormalize()
-                    self.reloadDataWithExistingSelection()
-
-                case .toggleFavorite:
-                    self.toggleFavoriteItems()
-
-                case .justClose:
-                    self.justClose()
-
-                case .closeAndPasteSelected:
-                    self.closeAndPaste()
-
-                case .removeSelected:
-                    self.removeSelectedItems()
-
-                default:
-                    break
-                }
-            })
+            .subscribe(onNext: onNext)
             .disposed(by: self.disposeBag)
+    }
+
+    func onNext(event: SearchViewEvents) {
+        switch event {
+        case .setSearchMode(let mode):
+            self.historyService.searchMode = mode
+            self.searchView.reloadData()
+            self.searchView.setSearchModeButton(mode: mode)
+
+        case .toggleSearchMode:
+            let mode = self.historyService.toggleSearchMode()
+            self.searchView.reloadData()
+            self.searchView.setSearchModeButton(mode: mode)
+
+        case .setTimeFilter(let seconds):
+            self.historyService.setTimeFilter(seconds: seconds)
+            self.searchView.reloadData()
+
+        case .toggleTimeFilter:
+            self.searchView.toggleTimeFilter()
+            self.historyService.setTimeFilter(seconds: nil)
+            self.searchView.reloadData()
+
+        case .cycleTheme:
+            self.prefs.cycleTheme()
+            self.searchView.applyTheme()
+            self.reloadDataWithExistingSelection()
+
+        case .toggleWrappingStrings:
+            self.prefs.useWrappingStrings.toggle()
+            self.updateSearchItemPreview()
+
+        case .toggleJoinStrings:
+            self.prefs.useJoinString.toggle()
+            self.updateSearchItemPreview()
+
+        case .toggleSearchScope:
+            self.historyService.favoritesOnly.toggle()
+            self.searchView.reloadData()
+            self.searchView.setSearchScopeButton(favoritesOnly: self.historyService.favoritesOnly)
+
+        case .togglePreview:
+            self.prefs.hidePreview.toggle()
+
+        case .scaleTextDown:
+            self.prefs.scaleTextDown()
+            self.reloadDataWithExistingSelection()
+
+        case .scaleTextUp:
+            self.prefs.scaleTextUp()
+            self.reloadDataWithExistingSelection()
+
+        case .scaleTextNormalize:
+            self.prefs.scaleTextNormalize()
+            self.reloadDataWithExistingSelection()
+
+        case .toggleFavorite:
+            self.toggleFavoriteItems()
+
+        case .justClose:
+            self.justClose()
+
+        case .closeAndPasteSelected:
+            self.closeAndPaste()
+
+        case .removeSelected:
+            self.removeSelectedItems()
+
+        default:
+            break
+        }
     }
     // swiftlint:enable cyclomatic_complexity
     // swiftlint:enable function_body_length
