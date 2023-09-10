37895c26f49b1176ae2f173df354749cee5bea6d
jasonm23 <jasonm23@gmail.com>
2023-09-10 23:56:26 +0700

    SearchAndPreviewViewBase: CutBoxBaseView

--- CutBox/CutBox/Source/App/SearchAndPreview/SearchAndPreviewViewBase.swift
+++ CutBox/CutBox/Source/App/SearchAndPreview/SearchAndPreviewViewBase.swift
@@ -11,7 +11,7 @@ import RxCocoa
 import Cocoa
 import CoreImage
 
-class SearchAndPreviewViewBase: NSView {
+class SearchAndPreviewViewBase: CutBoxBaseView {
 
     @IBOutlet weak var searchContainer: NSBox!
     @IBOutlet weak var searchTextContainer: NSBox!
