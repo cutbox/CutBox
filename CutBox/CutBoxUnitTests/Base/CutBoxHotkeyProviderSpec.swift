dfc7c25f6ddcab4e6f5c4d22ef64d8ca6c9dd548
jasonm23 <jasonm23@gmail.com>
2023-09-12 23:40:56 +0700

    Add spec for CutBoxHotkeyProvider

---
+++ CutBox/CutBoxUnitTests/Base/CutBoxHotkeyProviderSpec.swift
@@ -0,0 +1,48 @@
+//
+//  CutBoxHotkeyProviderSpec.swift
+//  CutBoxUnitTests
+//
+//  Created by jason on 12/9/23.
+//  Copyright © 2023 ocodo. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Magnet
+import Carbon
+
+class CutBoxHotkeyProviderSpec: QuickSpec {
+    @objc public func noopSelector() {}
+
+    override func spec() {
+        describe("CutBoxHotkeyProvider") {
+            describe("create") {
+                context("test mode") {
+                    it("should return nil ") {
+                        let keyCombo = KeyCombo(QWERTYKeyCode: kVK_ANSI_V,
+                                                      cocoaModifiers: [.shift, .command])!
+                        let provider = CutBoxHotkeyProvider()
+                        CutBoxHotkeyProvider.testMode = true
+                        let combo = provider.create(identifier: "Noop",
+                                                    keyCombo: keyCombo,
+                                                    target: self,
+                                                    action: #selector(self.noopSelector))
+                        expect(combo).to(beNil())
+                    }
+
+                    it("should return a valid object") {
+                        let keyCombo = KeyCombo(QWERTYKeyCode: kVK_ANSI_V,
+                                                cocoaModifiers: [.shift, .command])!
+                        let provider = CutBoxHotkeyProvider()
+                        CutBoxHotkeyProvider.testMode = false
+                        let combo = provider.create(identifier: "Noop",
+                                                    keyCombo: keyCombo,
+                                                    target: self,
+                                                    action: #selector(self.noopSelector))
+                        expect(combo).to(beAnInstanceOf(HotKey.self))
+                    }
+                }
+            }
+        }
+    }
+}
