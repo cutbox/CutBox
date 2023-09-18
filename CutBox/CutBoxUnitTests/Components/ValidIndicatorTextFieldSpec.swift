45727815692d5f025b54ab4aad6ec9de27085500
jasonm23 <jasonm23@gmail.com>
2023-09-18 10:26:03 +0700

    Complete spec for ValidIndicatorTextField

--- CutBox/CutBoxUnitTests/Components/ValidIndicatorTextFieldSpec.swift
+++ CutBox/CutBoxUnitTests/Components/ValidIndicatorTextFieldSpec.swift
@@ -12,7 +12,13 @@ import Nimble
 class TestValidIndicatorTextField: ValidIndicatorTextField {
     // This sub class exposes self.layer for property tests
     var fieldLayer: CALayer? {
-        return self.layer
+        get {
+            return self.layer
+        }
+
+        set {
+            self.layer = newValue
+        }
     }
 }
 
@@ -20,11 +26,30 @@ class ValidIndicatorTextFieldSpec: QuickSpec {
     override func spec() {
         describe("ValidIndicatorTextField") {
             let subject = TestValidIndicatorTextField()
-            it("should changed color when validity is set") {
+            subject.fieldLayer = CALayer()
+
+            it("should have a red border when invalid") {
                 subject.isValid = false
-                let redness = subject.fieldLayer?.borderColor?.components?[0]
-                let greenness = subject.fieldLayer?.borderColor?.components?[1]
-                expect(redness) > greenness
+                if let redness: CGFloat = subject.fieldLayer?.borderColor?.components?[0],
+                   let greenness: CGFloat = subject.fieldLayer?.borderColor?.components?[1] {
+                    // We don't care about the specific color or tint alteration
+                    // just that it is simplistically more red than green.
+                    expect(redness) > greenness
+                } else {
+                    fail("Could not access field layer border color components")
+                }
+            }
+
+            it("should have a green border when valid") {
+                subject.isValid = true
+                if let redness: CGFloat = subject.fieldLayer?.borderColor?.components?[0],
+                   let greenness: CGFloat = subject.fieldLayer?.borderColor?.components?[1] {
+                    // We don't care about the specific color or tint alteration
+                    // just that it is simplistically more green than red.
+                    expect(greenness) > redness
+                } else {
+                    fail("Could not access field layer border color components")
+                }
             }
         }
     }
