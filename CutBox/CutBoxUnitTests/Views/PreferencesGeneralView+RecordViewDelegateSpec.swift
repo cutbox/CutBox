6b7447c1a54af1ea17cb2b2f7d1d94e93919bf6d
jasonm23 <jasonm23@gmail.com>
2023-09-18 19:47:45 +0700

    Add PreferencesGeneralView+RecordViewDelegateSpec

---
+++ CutBox/CutBoxUnitTests/Views/PreferencesGeneralView+RecordViewDelegateSpec.swift
@@ -0,0 +1,28 @@
+//
+//  PreferencesGeneralView+RecordViewDelegateSpec.swift
+//  CutBoxUnitTests
+//
+//  Created by jason on 18/9/23.
+//  Copyright © 2023 ocodo. All rights reserved.
+//
+
+import Quick
+import Nimble
+import KeyHolder
+import Magnet
+
+class PreferencesGeneralView_RecordViewDelegateSpec: QuickSpec {
+    override func spec() {
+        describe("PreferencesGeneralView_RecordViewDelegate") {
+            let subject: PreferencesGeneralView = PreferencesGeneralView(frame: .zero)
+
+            it("returns true for can record key combo") {
+                let result = subject.recordView(
+                    RecordView(frame: .zero),
+                    canRecordKeyCombo: KeyCombo(key: .return, cocoaModifiers: .command)!)
+
+                expect(result) == true
+            }
+        }
+    }
+}
